d78177c616fd477792c072403347cb37abfb44ab
JANG <ysj1913@gmail.com>
2024-12-28 15:35:41 +0900

    테스트2

---
+++ project/finance_test/src/main/java/finance_test/testtttttttt.java
@@ -0,0 +1,14 @@
+package finance_test;
+
+public class testtttttttt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 1111;
+		
+		System.out.println(a);
+
+	}
+
+}
